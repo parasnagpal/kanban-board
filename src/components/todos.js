ec923db9ae13550b29db6890762f55a675740f48
Paras Nagpal <paras.nag@gmail.com>
2020-05-21 03:21:30 +0530

    Upgrading Component features

--- src/components/todos.js
+++ src/components/todos.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import {Row,Col} from 'reactstrap'
-import {MdDone} from 'react-icons/md'
-
-
+import {MdDone,MdAdd} from 'react-icons/md'
 
 
 class Todos extends React.Component{
     constructor(props){
         super(props)
+        this.state={
+            value:""
+        }
+        this.updateinputstate=this.updateinputstate.bind(this);
+        this.addtodo=this.addtodo.bind(this);
+    }
+    addtodo(){
+        let todo={
+            task:this.state.value,
+        }
+        this.props.addTodo(todo);
+        this.setState({
+            value:""
+        })
+    }
 
+    updateinputstate(e){
+        this.setState({
+            value:e.target.value
+        })
     }
 
     todo_card(data,index){
@@ -16,7 +33,7 @@ class Todos extends React.Component{
             <Row key={index}>
                 <Col>{data.task}</Col>
                 <Col xs="1">
-                    <div className='button' onClick={()=>this.props.onDone(index)}><MdDone/></div>
+                    <div className='button' onClick={()=>this.props.onDoing(index)}><MdDone/></div>
                 </Col>
             </Row>
         )
@@ -24,9 +41,18 @@ class Todos extends React.Component{
     render(){
         return(
             <React.Fragment>
-                {this.props.data.map((data,index)=>
-                  this.todo_card(data,index)
-                )}
+                <div>
+                    <Row>
+                        <Col className="underline"><h5>To-Do</h5></Col>
+                    </Row>
+                    {this.props.data.map((data,index)=>
+                      this.todo_card(data,index)
+                    )}
+                    </div>
+                <Row>
+                    <Col><input value={this.state.value} onChange={this.updateinputstate}/></Col>
+                    <Col sm="1"><div><MdAdd onClick={this.addtodo}/></div></Col>
+                </Row>
             </React.Fragment>
         );
     }
