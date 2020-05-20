edd42da4ade0415089c2df18afe4a4d5db859019
Paras Nagpal <paras.nag@gmail.com>
2020-05-21 02:03:41 +0530

    Component to manage todo tasks

--- src/components/todos.js
+++ src/components/todos.js
@@ -6,20 +6,26 @@ import {MdDone} from 'react-icons/md'
 
 
 class Todos extends React.Component{
+    constructor(props){
+        super(props)
 
-    todo_card(data){
+    }
+
+    todo_card(data,index){
         return (
-            <Row>
+            <Row key={index}>
                 <Col>{data.task}</Col>
-                <Col xs="1"><MdDone/></Col>
+                <Col xs="1">
+                    <div className='button' onClick={()=>this.props.onDone(index)}><MdDone/></div>
+                </Col>
             </Row>
         )
     }
     render(){
         return(
             <React.Fragment>
-                {this.props.data.map((data)=>
-                  this.todo_card(data)
+                {this.props.data.map((data,index)=>
+                  this.todo_card(data,index)
                 )}
             </React.Fragment>
         );
